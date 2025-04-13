f678d484b910991018f271e0701bb831c0d9dfc8
ibrahim <ebrahimamra.frontend@euruqalsahra.com>
2025-04-13 19:43:36 +0200

    fix: Update product list grid layout for improved responsiveness
    - Adjusted the grid layout in the ProductList component to enhance responsiveness and visual consistency.
    - Changed column settings to better accommodate varying screen sizes, ensuring a more user-friendly display of products.

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -136,7 +136,7 @@ export default async function ProductList({
           </p>
         </div>
       ) : (
-        <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+        <div className='grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5'>
           {products.map(product => (
             <ProductCard key={product.id} product={product} />
           ))}
